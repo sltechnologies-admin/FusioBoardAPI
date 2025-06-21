e9934daf665a07c0690f6aec9b944477d782ece6
KrishnaN <krishna.nekkala2013@gmail.com>
2025-06-21 17:52:24 +0530

    ignore files - gitignore

--- FilesAndContent/Queries_handy.sql
+++ FilesAndContent/Queries_handy.sql
@@ -1,4 +1,6 @@
-select * from Users
-select * from Projects
-select * from UserProjectRoles
-select * from logs
+select   * from Users
+select top 3 * from Projects
+select top 3 * from UserProjectRoles
+select top 3 * from logs
+
+-- Login Users set Username = 'adminuser', PasswordHash = 'sltech@123' where UserId = 1
